df68612e3d692a87a4f0fa71f99576c1df5eba3e
Svet <mursvyats@gmail.com>
2023-12-10 23:14:37 +0500

    Understood sliding window algorithm.

--- Neetcode/Sliding_Window/3.Longest_Substring_wo_rep_char/solution.js
+++ Neetcode/Sliding_Window/3.Longest_Substring_wo_rep_char/solution.js
@@ -2,6 +2,22 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-    
+let lengthOfLongestSubstring = function(s) {
+    let [i, j, max] = [0, 0, 0];
+    let set = new Set();
+    while (i < s.length) {
+        let char = s.charAt(i);
+        while (set.has(char)) {
+            set.delete(s.charAt(j));
+            ++j;
+        }
+        set.add(char);
+        max = Math.max(max, i - j + 1);
+        ++i;
+    }
+    return max;
 };
+
+s = "pwwkewaybhjk"
+
+console.log(lengthOfLongestSubstring(s));
