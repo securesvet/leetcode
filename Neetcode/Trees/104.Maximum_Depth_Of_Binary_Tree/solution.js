c07a50092c5391e61299f26aee75bbe92da08837
Svet <mursvyats@gmail.com>
2023-12-16 13:45:16 +0500

    104 solved

---
+++ Neetcode/Trees/104.Maximum_Depth_Of_Binary_Tree/solution.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const maxDepth = function(root) {
+    if (root === null) return 0;
+    const leftDepth = maxDepth(root.left);
+    const rightDepth = maxDepth(root.right);
+    return Math.max(leftDepth, rightDepth) + 1;
+};
