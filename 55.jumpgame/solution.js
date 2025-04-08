61774bdd7d52385de4e6edc0dc7251898bdaf112
Sviatoslav Murzin <mursvyats@gmail.com>
2025-04-08 22:03:10 +0500

    new: added 55.jumpgame

---
+++ 55.jumpgame/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function (nums) {
+  if ([0, 1].includes(nums.length)) return true;
+
+  let currentSegmentLength = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (i > currentSegmentLength) {
+      return false;
+    }
+    currentSegmentLength = Math.max(currentSegmentLength, i + nums[i]);
+    if (currentSegmentLength >= nums.length) return true;
+  }
+};
+
+const arr = [1, 0, 1, 0];
+
+console.log(canJump(arr));
